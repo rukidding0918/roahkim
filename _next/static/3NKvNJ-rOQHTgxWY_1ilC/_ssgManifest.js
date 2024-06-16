eeac20dde4be3c011316efa370d3e308310ff208
Hyoung Kyoo Kim <110450164+rukidding0918@users.noreply.github.com>
2024-06-16 02:12:12 +0000

    Updates

--- _next/static/3NKvNJ-rOQHTgxWY_1ilC/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fworks","\u002Fworks\u002F[year]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- js/script.js
+++
@@ -1,8 +0,0 @@
-const atags = document.querySelectorAll('.uk-nav > li > a');
-const offcanvas = document.getElementById('offcanvas-push');
-// a tag 클릭시 offcanvas 닫기
-atags.forEach(atag => {
-  atag.addEventListener('click', () => {
-    UIkit.offcanvas(offcanvas).hide();
-  });
-});
